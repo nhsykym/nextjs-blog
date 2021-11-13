e77307b89bdaca2a6c43882176d7fa080331b5ba
Naohisa Yokoyama <nhsykym@gmail.com>
2021-11-13 16:10:58 +0900

    modify index.js

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Naohisa.Hello, I'm Naohisa.Hello, I'm Naohisa.Hello, I'm Naohisa.</p>
+        <p>Hi, I'm Naohisa. Hi, I'm Naohisa. Hi, I'm Naohisa. Hi, I'm Naohisa. </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
